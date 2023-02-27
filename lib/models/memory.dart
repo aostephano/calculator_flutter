cfe4e33040460bf5545924393408bdfcda8d670a
Alexandre Stephano <ale.stephano@gmail.com>
2023-02-27 10:04:47 -0300

    Replacing operators and ending the project

--- lib/models/memory.dart
+++ lib/models/memory.dart
@@ -1,5 +1,3 @@
-import 'package:flutter/cupertino.dart';
-
 class Memory {
   static const operations = const ['%', '/', 'x', '-', '+', '='];
   final _buffer = [0.0, 0.0];
@@ -19,6 +17,11 @@ class Memory {
 
   //Methods
   void addCommand(command) {
+    //If the user use another command, the actual operation are replaced
+    if (_isReplacingOperation(command)) {
+      _operation = command;
+      return;
+    }
     if (command == "AC") {
       _allClear();
     } else if (operations.contains(command)) {
@@ -28,10 +31,13 @@ class Memory {
     }
 
     _lastOperator = command;
+  }
 
-    _isReplacingOpetation(String command) {
-      return;
-    }
+  _isReplacingOperation(String command) {
+    return operations.contains(_lastOperator) &&
+        operations.contains(command) &&
+        _lastOperator != "=" &&
+        command != "=";
   }
 
   _addDigit(String digit) {
@@ -58,18 +64,22 @@ class Memory {
   _setOperation(String newOperation) {
     bool isEqualSign = newOperation == '=';
     if (_bufferIndex == 0) {
+      //Only clean the buffer display if isnt a "=" Equal Operator
       if (!isEqualSign) {
         _operation = newOperation;
         _bufferIndex = 1;
         _wipeValue = true;
       }
     } else {
-      _buffer[0] = _calculate();
+      _buffer[0] =
+          _calculate(); //Atributte the result in the first buffer, and clear the next one to be used
       _buffer[1] = 0.0;
       _value = _buffer[0].toString();
       _value = _value.endsWith(".0") ? _value.split('.')[0] : _value;
 
       _operation = isEqualSign ? null : newOperation;
+      //_bufferIndex set to 0 = U dont want new Operations
+      // _bufferIndex set to 1 = U and to add more Operations
       _bufferIndex = isEqualSign ? 0 : 1;
     }
     _wipeValue = true;
@@ -78,9 +88,11 @@ class Memory {
   //AC
   void _allClear() {
     _value = "0";
+    //Clear both buffers
     _buffer[0] = 0.0;
     _buffer[1] = 0.0;
     _operation = null;
+    //Reset buffer
     _bufferIndex = 0;
     _wipeValue = false;
   }
